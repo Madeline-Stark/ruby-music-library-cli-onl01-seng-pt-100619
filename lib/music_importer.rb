cd3097166df61e86ad4b113eb77713595ec8d515
github username <github email address>
2020-10-13 17:35:21 -0700

    Done.

--- lib/music_importer.rb
+++ lib/music_importer.rb
@@ -6,4 +6,19 @@ class MusicImporter
       @path = path
     end
 
+    def files
+        filename_array = Dir.glob("#{path}/*.mp3")
+        filename_array.collect do |filename|
+            filename.gsub("#{path}/", "")
+        end
+
+        # memoization
+        # uses less memory
+        # @files ||= Dir.glob("#{path}/*.mp3").collect{ |f| f.gsub("#{path}/", "") }
+    end
+
+    def import
+        files.each{ |f| Song.create_from_filename(f) }
+    end
+
 end 
